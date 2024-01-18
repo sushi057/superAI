f913752fdf07ea5ecfb9a38e4aab578ea1b575ff
Suvash Joshi <suvash2077@gmail.com>
2024-01-19 01:49:54 +0545

    Fixed second graph sizing

--- src/components/Charts.tsx
+++ src/components/Charts.tsx
@@ -14,36 +14,40 @@ function Charts() {
     },
     grid: [
       {
-        left: "2%",
-        right: "2%",
-        top: "-2%",
-        bottom: "0%",
-        height: "100%",
-        width: "50%",
-        containLabel: true,
+        left: "12%",
+        right: "5%",
+        width: "35%",
+        top: "2%",
       },
       {
-        left: "2%",
-        right: "2%",
-        top: "-2%",
-        bottom: "0%",
-        height: "100%",
-        width: "50%",
-        containLabel: true,
+        top: "2%",
+        left: "55%",
+        rigth: "-20%",
+        // width: "45%",
       },
     ],
     xAxis: [
       {
         type: "value",
+        axisLabel: {
+          formatter: "{value} %",
+        },
         gridIndex: 0,
       },
       {
         type: "value",
-        position: "top",
+        data: [-20, -10, 0, 10, 20],
         splitLine: {
           lineStyle: {
             type: "dashed",
           },
+        },
+        axisLine: {
+          show: true,
+        },
+        boundaryGap: ["10%", "5%"],
+        axisLabel: {
+          formatter: "{value} pp",
         },
         gridIndex: 1,
       },
@@ -77,7 +81,7 @@ function Charts() {
       {
         name: "Blue",
         type: "bar",
-        data: [16.6, 12.9, 21.25, 17.5, 15.0, 16.0, 18.0, 20.0, 19.0],
+        data: [9.37, 11, 11.9, 12.3, 12.5, 12.9, 13.7, 15.3, 16.6],
         itemStyle: {
           color: "#4e73df",
         },
@@ -87,13 +91,13 @@ function Charts() {
           formatter: "{c}%",
           color: "#4e73df",
         },
-        barWidth: 12,
+        barMaxWidth: 20,
         barCategoryGap: "20%",
       },
       {
         name: "Red",
         type: "bar",
-        data: [16.6, 7.5, 10.0, 8.0, 7.5, 8.0, 9.0, 10.0, 9.5],
+        data: [8.59, 0, 10.9, 12, 9.27, 20.8, 11.6, 18.1, 16.6],
         itemStyle: {
           color: "#e74a3b",
         },
@@ -103,29 +107,34 @@ function Charts() {
           formatter: "{c}%",
           color: "#e74a3b",
         },
-        barWidth: 12,
+        barMaxWidth: 20,
         barGap: "20%",
         barCategoryGap: "20%",
       },
       {
         name: "Difference",
         type: "bar",
-        stack: "Difference",
+        stack: "Total",
         label: {
           show: true,
+          position: "outside",
+          color: "#50C878",
           formatter: "{c} pp",
         },
         data: [
-          { value: 0.1 },
-          { value: -2.87, label: labelRight },
-          { value: 2.04 },
-          { value: -7.9, label: labelRight },
-          { value: 3.2 },
-          { value: 0.34 },
-          { value: 1.08 },
-          { value: 11.0 },
           { value: 0.78 },
+          { value: 11.0 },
+          { value: 1.08 },
+          { value: 0.34 },
+          { value: 3.2 },
+          { value: -7.9, label: labelRight },
+          { value: 2.04 },
+          { value: -2.87, label: labelRight },
+          { value: 0.1 },
         ],
+        itemStyle: {
+          color: "#50C878",
+        },
         xAxisIndex: 1,
         yAxisIndex: 1,
         barMaxWidth: 40,
@@ -134,10 +143,10 @@ function Charts() {
   };
 
   return (
-    <div className="flex-1">
+    <div className="h-full w-full flex-1">
       <ReactECharts
         option={horizontalBarGraphOption}
-        style={{ height: "100%" }}
+        style={{ height: "100%", width: "100%" }}
       />
     </div>
   );
